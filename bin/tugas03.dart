c44522f4f1899608a200696700bcbdf4fac642f2
chrmyill <rommysidartha@gmail.com>
2024-03-28 09:58:58 +0700

    update

---
+++ bin/tugas03.dart
@@ -0,0 +1,13 @@
+import'dart:io';
+
+void main(){
+  var x=3;
+  var y=2;
+
+  for(int i=1; i<=y; i++){
+    for(int i=1; i<=x; i++){
+      stdout.write('x');
+    }
+    stdout.write("\n");
+  }
+}
