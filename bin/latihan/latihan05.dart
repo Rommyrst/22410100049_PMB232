88420283c96daf30efd8d879f8a003de67705dc6
chrmyill <rommysidartha@gmail.com>
2024-04-03 22:19:42 +0700

    update

---
+++ bin/latihan/latihan05.dart
@@ -0,0 +1,27 @@
+void main()
+{
+  //list
+  var x = [2,3,4];
+
+  //set
+  var nama = <String>{"adi","budi","cindi"};
+  var namaa = new Set();
+
+  //map
+  var job = {'nama':'lala','kerja':'programmer'};
+  var jobb = new Map();
+
+  x.add(5);
+  print(x[2]);
+  print(x.length);
+  x.removeAt(1);
+  print(x.length);
+  print(nama);
+  print(nama.elementAt(1));
+  print(job['kerja']);
+}
+
+
+/*
+tugas 5-12 ada di video youtube
+ */
